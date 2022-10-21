b8a9a702fcf1fa199b7ff5afca6a5f685bb43d59
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-21 20:21:56 -0300

    feat: create module address

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { AddressModule } from './modules/address/address.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
