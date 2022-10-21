f72a8324b6caa4a921c0a27f08009a3fda635b03
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-21 18:23:03 -0300

    feat: create handle error

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -1,43 +1,52 @@
 import { PrismaClient } from '@prisma/client';
+import { handleError } from 'src/shared/utils/handle-error.util';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UpdateUserDto } from '../dtos/update-user.sto';
 import { UserEntity } from '../entity/user.entity';
 
 export class UserRepository extends PrismaClient {
   async createUser(data: CreateUserDto): Promise<UserEntity> {
-    return this.user.create({ data });
+    return this.user.create({ data }).catch(handleError);
   }
 
   async findAllUsers(): Promise<UserEntity[]> {
-    return this.user.findMany();
+    return this.user.findMany().catch(handleError);
   }
 
   async findUserByEmail(email: string): Promise<UserEntity> {
-    return this.user.findUnique({
-      where: { email },
-    });
+    return this.user
+      .findUnique({
+        where: { email },
+      })
+      .catch(handleError);
   }
 
   async findOneById(id: number): Promise<UserEntity> {
-    return this.user.findUnique({
-      where: { id },
-    });
+    return this.user
+      .findUnique({
+        where: { id },
+      })
+      .catch(handleError);
   }
 
   async updateUserById(id: number, data: UpdateUserDto): Promise<UserEntity> {
-    return await this.user.update({
-      where: {
-        id,
-      },
-      data,
-    });
+    return await this.user
+      .update({
+        where: {
+          id,
+        },
+        data,
+      })
+      .catch(handleError);
   }
 
   async deleteUserById(id: number): Promise<void> {
-    await this.user.delete({
-      where: {
-        id,
-      },
-    });
+    await this.user
+      .delete({
+        where: {
+          id,
+        },
+      })
+      .catch(handleError);
   }
 }
